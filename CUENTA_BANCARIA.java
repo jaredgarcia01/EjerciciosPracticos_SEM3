a98485dc327bea8e23d890190691f566faa32975
jaredgarcia01 <101343666+jaredgarcia01@users.noreply.github.com>
2022-04-01 20:48:03 +0200

    Add files via upload

---
+++ CUENTA_BANCARIA.java
@@ -0,0 +1,30 @@
+package cuenta_bancaria;
+
+public class CUENTA_BANCARIA {
+
+
+    long numeroCuenta;
+    Cliente propietarioCuenta;
+   double saldoCuenta;
+    
+    public void ABONAR(double cantidad){
+        saldoCuenta+= cantidad;//logica de negocio
+        
+    }
+    public void RETIRAR(double cantidad){
+        if(cantidad > saldoCuenta){
+            System.out.println("Saldo insuficiente");
+        }
+        saldoCuenta-= cantidad;//logica de negocio
+    }
+    
+    public String DATOSPROPIETARIOCUENTA(){
+        String informacion="";
+        informacion += "DUI: " + propietarioCuenta.DUICLIENTE + "\n";
+        informacion += "NONMBRE: " + propietarioCuenta.NOMBRECLIENTE + "\n";
+        informacion += "TELEFONO: " + propietarioCuenta.TELEFONOCLIENTE + "\n";
+        return informacion;
+    }
+   
+    }
+    
