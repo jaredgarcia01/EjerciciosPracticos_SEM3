e6c2a4925966a2bba2bb6f71fdb1b557a7d8aba3
jaredgarcia01 <101343666+jaredgarcia01@users.noreply.github.com>
2022-04-01 21:25:25 +0200

    Add files via upload

---
+++ NOTAS.java
@@ -0,0 +1,44 @@
+package NOTAS;
+
+public class NOTAS {
+    
+    double notas1;
+     double notas2;
+     double notas3;
+    
+    public NOTAS(){
+        
+    }
+
+    public NOTAS(double notas1, double notas2, double notas3) {
+        this.notas1 = notas1;
+        this.notas2 = notas2;
+        this.notas3 = notas3;
+    }
+
+    public double getNOTAS1() {
+        return notas1;
+    }
+
+    public void setNOTAS1(double nota1) {
+        this.notas1 = notas1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public double getNota3() {
+        return nota3;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+    
+    
+}
